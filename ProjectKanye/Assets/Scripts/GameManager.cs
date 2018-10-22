1246ab7ca060fdbd01d1b75c173d12fc6f5ec9d6
Jose The Tech Monk <deschamps.jose001@gmail.com>
2018-10-22 13:52:02 -0400

    Stage 1
    The beginning start of developing.

---
+++ ProjectKanye/Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    
+
+    [Header("Kanye Answers")]
+    public string[] kanyeAnswers;
+    public Text[] choiceText;
+    public Button[] choiceButton;
+    private int answerIndex = 0;
+
+
+
+
+
+
+
+
+
+    private void Start()
+    {
+
+
+    }
+
+
+   private  void Update()
+    {
+
+       
+    }
+
+
+
+    public void FirstAnswerBubble(){
+
+        switch (answerIndex)
+        {
+            case 0:
+                
+                break;
+
+            case 1:
+
+                break;
+
+
+
+            default:
+                break;
+        }
+
+
+    }
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+}
